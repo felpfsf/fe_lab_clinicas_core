09e4d6505fe2c5c99d2d4b7094a748a6ef7b2d00
Felipe Ferreira <feferreira.sf@gmail.com>
2024-01-30 16:59:58 -0300

    ✨ core config

---
+++ lib/src/lab_clinicas_core_config.dart
@@ -0,0 +1,47 @@
+import 'package:asyncstate/asyncstate.dart';
+import 'package:fe_lab_clinicas_core/src/loader/lab_clinicas_loader.dart';
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_getit/flutter_getit.dart';
+
+class LabClinicasCoreConfig extends StatelessWidget {
+  final ApplicationBindings? binding;
+  final List<FlutterGetItPageRouter>? pages;
+  final List<FlutterGetItPageBuilder>? pagesBuilder;
+  final List<FlutterGetItModule>? modules;
+  final String title;
+
+  const LabClinicasCoreConfig({
+    super.key,
+    this.binding,
+    this.pages,
+    this.pagesBuilder,
+    this.modules,
+    required this.title,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return FlutterGetIt(
+      debugMode: kDebugMode,
+      bindings: binding,
+      pages: [...pages ?? [], ...pagesBuilder ?? []],
+      modules: modules,
+      builder: (context, routes, flutterGetItNavObserver) {
+        return AsyncStateBuilder(
+          loader: LabClinicasLoader(),
+          builder: (navigatorObserver) {
+            return MaterialApp(
+              navigatorObservers: [
+                navigatorObserver,
+                flutterGetItNavObserver,
+              ],
+              routes: routes,
+              title: title,
+            );
+          },
+        );
+      },
+    );
+  }
+}
