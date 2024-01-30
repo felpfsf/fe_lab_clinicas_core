a31369ba9fb43acf6f4a3918db60405d12e8fcf2
Felipe Ferreira <feferreira.sf@gmail.com>
2024-01-30 16:52:26 -0300

    ✨ export core config file

--- lib/fe_lab_clinicas_core.dart
+++ lib/fe_lab_clinicas_core.dart
@@ -5,4 +5,5 @@ export 'src/fb/either.dart';
 export 'src/fb/nil.dart';
 export 'src/fb/unit.dart';
 export 'src/helpers/messages.dart';
+export 'src/lab_clinicas_core_config.dart';
 export 'src/rest_client/rest_client.dart';
