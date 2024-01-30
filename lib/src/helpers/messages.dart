fb381582fc4bef55bcceba63978fa3ab0b3b7f84
Felipe Ferreira <feferreira.sf@gmail.com>
2024-01-30 15:18:11 -0300

    ✨ creating mixins with signals to interact with view and controller(view model)

--- lib/src/helpers/messages.dart
+++ lib/src/helpers/messages.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/cupertino.dart';
+import 'package:signals_flutter/signals_flutter.dart';
 import 'package:top_snackbar_flutter/custom_snack_bar.dart';
 import 'package:top_snackbar_flutter/top_snack_bar.dart';
 
@@ -22,5 +23,68 @@ final class Messages {
       Overlay.of(context),
       CustomSnackBar.success(message: message),
     );
+  }
+}
+
+// mixins
+// Irão fazer a integração com a view e a controller
+
+// Esse mixing irá notificar tendo o controle da mensagem
+mixin MessageStateMixin {
+  final Signal<String?> _errorMessage = signal(null);
+  String? get errorMessage => _errorMessage();
+
+  final Signal<String?> _infoMessage = signal(null);
+  String? get infoMessage => _infoMessage();
+
+  final Signal<String?> _successMessage = signal(null);
+  String? get successMessage => _successMessage();
+
+  void clearError() => _errorMessage.value = null;
+  void clearInfo() => _errorMessage.value = null;
+  void clearSuccess() => _errorMessage.value = null;
+
+  void showError(String message) {
+    untracked(() => clearError());
+    _errorMessage.value = message;
+  }
+
+  void showInfo(String message) {
+    untracked(() => clearInfo());
+    _infoMessage.value = message;
+  }
+
+  void showSuccess(String message) {
+    untracked(() => clearSuccess());
+    _successMessage.value = message;
+  }
+
+  void clearAllMessages() {
+    untracked(() {
+      clearError();
+      clearInfo();
+      clearSuccess();
+    });
+  }
+}
+
+// Esse mixin estará exibindo as notificações
+mixin MessageViewMixin<T extends StatefulWidget> on State<T> {
+  void messageListener(MessageStateMixin state) {
+    effect(() {
+      switch (state) {
+        case MessageStateMixin(:final errorMessage?):
+          Messages.showError(errorMessage, context);
+
+        case MessageStateMixin(:final infoMessage?):
+          Messages.showInfo(infoMessage, context);
+
+        case MessageStateMixin(:final successMessage?):
+          Messages.showSuccess(successMessage, context);
+
+          break;
+        default:
+      }
+    });
   }
 }
