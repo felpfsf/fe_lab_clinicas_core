30c8a029538aea1f0e207b45f7572eb8158f04df
Felipe Ferreira <feferreira.sf@gmail.com>
2024-01-30 15:36:21 -0300

    ✨ export rest client and local storage

--- lib/fe_lab_clinicas_core.dart
+++ lib/fe_lab_clinicas_core.dart
@@ -1,6 +1,8 @@
 library fe_lab_clinicas_core;
 
+export 'src/constants/local_storage_constants.dart';
 export 'src/fb/either.dart';
 export 'src/fb/nil.dart';
 export 'src/fb/unit.dart';
 export 'src/helpers/messages.dart';
+export 'src/rest_client/rest_client.dart';
