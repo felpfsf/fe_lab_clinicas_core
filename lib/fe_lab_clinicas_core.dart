ee72b12043e0d289cac040f2fea229ce07ddae35
Felipe Ferreira <feferreira.sf@gmail.com>
2024-02-01 15:30:30 -0300

    ✨ exceptions

--- lib/fe_lab_clinicas_core.dart
+++ lib/fe_lab_clinicas_core.dart
@@ -1,9 +1,17 @@
 library fe_lab_clinicas_core;
 
 export 'src/constants/local_storage_constants.dart';
+
 export 'src/fb/either.dart';
 export 'src/fb/nil.dart';
 export 'src/fb/unit.dart';
+
 export 'src/helpers/messages.dart';
+
 export 'src/lab_clinicas_core_config.dart';
+
 export 'src/rest_client/rest_client.dart';
+
+export 'src/exceptions/auth_exception.dart';
+export 'src/exceptions/repository_exception.dart';
+export 'src/exceptions/service_exception.dart';
