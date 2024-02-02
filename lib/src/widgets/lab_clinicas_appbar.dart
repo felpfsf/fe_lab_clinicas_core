32d293cdb0306615d78207f5c0c745b19f87bf6a
Felipe Ferreira <feferreira.sf@gmail.com>
2024-02-02 12:24:54 -0300

    ✨ appbar widgets

---
+++ lib/src/widgets/lab_clinicas_appbar.dart
@@ -0,0 +1,28 @@
+import 'package:fe_lab_clinicas_core/src/theme/lab_clinicas_theme.dart';
+import 'package:flutter/material.dart';
+
+class LabClinicasAppbar extends AppBar {
+  LabClinicasAppbar({super.key, List<Widget>? actions})
+      : super(
+          toolbarHeight: 72,
+          backgroundColor: Colors.transparent,
+          flexibleSpace: Container(
+            alignment: Alignment.centerLeft,
+            decoration: BoxDecoration(
+              color: LabClinicasTheme.primaryElement,
+              boxShadow: [
+                BoxShadow(
+                  color: Colors.black.withOpacity(0.5),
+                  offset: Offset(0, 1),
+                  blurRadius: 4,
+                ),
+              ],
+            ),
+            child: Padding(
+              padding: const EdgeInsets.only(left: 64),
+              child: Image.asset('assets/images/logo_horizontal.png'),
+            ),
+          ),
+          actions: actions,
+        );
+}
